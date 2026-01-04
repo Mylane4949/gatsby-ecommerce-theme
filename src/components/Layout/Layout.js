53c06f4dd763753bc6420e787feda94750bce296
Mylane4949 <samaryoh@gmail.com>
2026-01-04 16:13:34 -0600

    Update Layout.js

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,6 +1,20 @@
-<Helmet>
+import React from 'react';
+import Helmet from 'react-helmet';
+import PropTypes from 'prop-types';
+
+import Header from '../Header';
+import Footer from '../Footer';
+import * as styles from './Layout.module.css';
+
+// CSS not modular here to provide global styles
+import './Globals.css';
+
+const Layout = ({ children, disablePaddingBottom = false }) => {
+  return (
+    <>
+      <Helmet>
         <title>My Store</title>
-        {/* Load the Netlify Identity Widget Script */}
+        {/* Load the Identity Widget */}
         <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
         
         <link
@@ -11,11 +25,11 @@
         <link
           rel="stylesheet"
           type="text/css"
-          charSet="UTF-8"
+          charSet="UTF-8" 
           href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
         />
 
-        {/* The Redirect Logic */}
+        {/* The Redirect Script - Wrapped correctly for React */}
         <script>
           {`
             if (window.netlifyIdentity) {
@@ -30,3 +44,22 @@
           `}
         </script>
       </Helmet>
+
+      <Header />
+      <main
+        className={`${styles.main} ${
+          disablePaddingBottom === true ? styles.disablePaddingBottom : ''
+        }`}
+      >
+        {children}
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Layout;

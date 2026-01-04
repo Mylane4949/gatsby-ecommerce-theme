ddc9f07d49c7b08b171f8bca3b4df32cb34d9527
Mylane4949 <samaryoh@gmail.com>
2026-01-04 17:09:33 -0600

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,21 +3,22 @@ module.exports = {
     title: `My Ecommerce Store`,
   },
   plugins: [
-    // 1. Tell Gatsby to look at your CMS products folder
+    // --- THIS IS THE NEW SECTION ---
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `products`,
-        path: `${__dirname}/content/products`, // This must match your config.yml folder
+        path: `${__dirname}/content/products`, 
       },
     },
-    // 2. This turns the markdown files from the CMS into data Gatsby can use
     `gatsby-transformer-remark`,
-    // 3. This handles your images so they load fast
+    // ------------------------------
+
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
-    // 4. Netlify specific plugin (helps with redirects and headers)
     `gatsby-plugin-netlify`,
+    
+    // ... if your template had other plugins, leave them here!
   ],
 }

8e31ac23db9ce42cb7d6113bc18641bc0764777a
Mylane4949 <samaryoh@gmail.com>
2026-01-04 19:29:07 -0600

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,8 +11,13 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-  ],
-};
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `products`,
+        path: `${__dirname}/content/products`,
+      },
+    },
     `gatsby-transformer-remark`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
@@ -20,16 +25,3 @@ module.exports = {
     `gatsby-plugin-netlify`,
   ],
 };
-const { createFilePath } = require(`gatsby-source-filesystem`)
-
-exports.onCreateNode = ({ node, getNode, actions }) => {
-  const { createNodeField } = actions
-  if (node.internal.type === `MarkdownRemark`) {
-    const slug = createFilePath({ node, getNode, basePath: `pages` })
-    createNodeField({
-      node,
-      name: `slug`,
-      value: slug,
-    })
-  }
-}

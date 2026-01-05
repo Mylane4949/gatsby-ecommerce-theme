5e9f69a612e3e4748b6e6f581d147e70fddecbf0
Mylane4949 <samaryoh@gmail.com>
2026-01-04 19:06:59 -0600

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,19 @@
-// https://stackoverflow.com/questions/63124432/how-do-i-configure-mini-css-extract-plugin-in-gatsby
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+// 1. This part creates the "slugs" (the URLs) for your products
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
+}
+
+// 2. This part fixes CSS ordering issues (from your StackOverflow link)
 exports.onCreateWebpackConfig = (helper) => {
   const { stage, actions, getConfig } = helper;
   if (stage === 'develop' || stage === 'build-javascript') {

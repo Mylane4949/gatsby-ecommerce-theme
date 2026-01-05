ed3b6c3d74844a4a06d569a24eb6717222aae82a
Mylane4949 <samaryoh@gmail.com>
2026-01-04 18:09:22 -0600

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,3 +17,16 @@ module.exports = {
     `gatsby-plugin-netlify`,
   ],
 }
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
+}

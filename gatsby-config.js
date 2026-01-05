56ee224416c819c89c1d10ef4b9e3b19829daf16
Mylane4949 <samaryoh@gmail.com>
2026-01-04 19:57:50 -0600

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,3 +16,29 @@ module.exports = {
     `gatsby-transformer-sharp`,
   ],
 };
+const path = require(`path`)
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const result = await graphql(`
+    query {
+      allMarkdownRemark {
+        nodes {
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  `)
+
+  result.data.allMarkdownRemark.nodes.forEach((node) => {
+    createPage({
+      path: node.fields.slug,
+      component: path.resolve(`./src/templates/product.js`),
+      context: {
+        slug: node.fields.slug,
+      },
+    })
+  })
+}

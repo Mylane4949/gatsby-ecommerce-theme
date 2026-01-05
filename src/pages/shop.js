c71c2f295184dbdb84ad24fede174f9a54fde827
Mylane4949 <samaryoh@gmail.com>
2026-01-04 19:20:20 -0600

    Update shop.js

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -15,12 +15,23 @@ const ShopPage = ({ data }) => {
     imageAlt: product.frontmatter.title,
   }));
 
+  const ShopPage = ({ data }) => {
+  // 1. Get your real products from the CMS data
+  const products = data.allMarkdownRemark.nodes;
+
   return (
     <Layout>
-      <div className={styles.productContainer}>
-        {/* 3. Use 'formattedProducts' here */}
-        {formattedProducts.map((item, index) => (
-          <ProductCard key={index} {...item} />
+      <div className="product-grid">
+        {/* 2. Loop through each product and fill the ProductCard "mold" */}
+        {products.map((node) => (
+          <ProductCard 
+            key={node.fields.slug}
+            name={node.frontmatter.title}
+            price={node.frontmatter.price}
+            image={node.frontmatter.image}
+            imageAlt={node.frontmatter.title}
+            slug={node.fields.slug} // This sends the URL to the card
+          />
         ))}
       </div>
     </Layout>
